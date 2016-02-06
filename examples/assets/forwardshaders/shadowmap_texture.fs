0ab5f6f054f7bf8fff227cda3f20820e3f098d7e
Timothy Bogdala <tdb@animal-machine.com>
2016-02-05 22:23:51 -0600

    fixed shader compilation error with texture2D

--- examples/assets/forwardshaders/shadowmap_texture.fs
+++ examples/assets/forwardshaders/shadowmap_texture.fs
@@ -11,6 +11,6 @@ out vec4 frag_color;
 void main (void) {
 	float f=50.0;
 	float n = 0.5;
-	float z = (2 * n) / (f + n - texture2D(MATERIAL_TEX_0, vs_tex0_uv).x * (f - n));
+	float z = (2 * n) / (f + n - texture(MATERIAL_TEX_0, vs_tex0_uv).x * (f - n));
 	frag_color = MATERIAL_DIFFUSE * vec4(z, z, z, 1.0);
 }
