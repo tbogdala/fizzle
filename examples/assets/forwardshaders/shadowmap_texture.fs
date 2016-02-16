ff17839d55f1a9c20da09fb688972e0ab1afc9e9
Timothy Bogdala <tdb@animal-machine.com>
2016-02-15 20:15:51 -0600

    commited merged file for real this time

--- examples/assets/forwardshaders/shadowmap_texture.fs
+++ examples/assets/forwardshaders/shadowmap_texture.fs
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-#version 330
-precision highp float;
-
-uniform sampler2D MATERIAL_TEX_0;
-uniform vec4 MATERIAL_DIFFUSE;
-
-in vec2 vs_tex0_uv;
-
-out vec4 frag_color;
-
-void main (void) {
-	float f=50.0;
-	float n = 0.5;
-	float z = (2 * n) / (f + n - texture2D(MATERIAL_TEX_0, vs_tex0_uv).x * (f - n));
-	frag_color = MATERIAL_DIFFUSE * vec4(z, z, z, 1.0);
-}
-=======
 #version 330
 precision highp float;
 
@@ -32,4 +14,3 @@ void main (void) {
 	float z = (2 * n) / (f + n - texture(MATERIAL_TEX_0, vs_tex0_uv).x * (f - n));
 	frag_color = MATERIAL_DIFFUSE * vec4(z, z, z, 1.0);
 }
->>>>>>> 0ab5f6f054f7bf8fff227cda3f20820e3f098d7e
