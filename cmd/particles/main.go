d14cb2c64a473cbaea2971c1cb6f70cea8bcece1
Timothy Bogdala <tdb@animal-machine.com>
2016-06-26 15:11:38 -0500

    initial test implementation of particles and the particle editor using eweygewey

---
+++ cmd/particles/main.go
@@ -0,0 +1,277 @@
+// Copyright 2016, Timothy` Bogdala <tdb@animal-machine.com>
+// See the LICENSE file for more details.
+
+package main
+
+import (
+	"flag"
+	"math"
+	"runtime"
+	"time"
+
+	glfw "github.com/go-gl/glfw/v3.1/glfw"
+	mgl "github.com/go-gl/mathgl/mgl32"
+	gui "github.com/tbogdala/eweygewey"
+	guiinput "github.com/tbogdala/eweygewey/glfwinput"
+
+	fizzle "github.com/tbogdala/fizzle"
+	graphics "github.com/tbogdala/fizzle/graphicsprovider"
+	opengl "github.com/tbogdala/fizzle/graphicsprovider/opengl"
+	particles "github.com/tbogdala/fizzle/particles"
+	forward "github.com/tbogdala/fizzle/renderer/forward"
+)
+
+var (
+	windowWidth       = 1280
+	windowHeight      = 720
+	mainWindow        *glfw.Window
+	uiman             *gui.Manager
+	billboardFilepath = "../../examples/assets/textures/explosion00.png"
+)
+
+const (
+	fontScale    = 18
+	fontFilepath = "../../examples/assets/HammersmithOne.ttf"
+	fontGlyphs   = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890., :[]{}\\|<>;\"'~`?/-+_=()*&^%$#@!"
+)
+
+// block of flags set on the command line
+var (
+	flagDesktopNumber int
+)
+
+// GLFW event handling must run on the main OS thread. If this doesn't get
+// locked down, you will likely see random crashes on memory access while
+// running the application after a few seconds.
+//
+// So on initialization of the module, lock the OS thread for this goroutine.
+func init() {
+	runtime.LockOSThread()
+	flag.IntVar(&flagDesktopNumber, "desktop", -1, "the index of the desktop to create the main window on")
+}
+
+func main() {
+	// parse the command line options
+	flag.Parse()
+
+	// start off by initializing the GL and GLFW libraries and creating a window.
+	w, gfx := initGraphics("Simple Cube", windowWidth, windowHeight)
+	mainWindow = w
+
+	/////////////////////////////////////////////////////////////////////////////
+	// create and initialize the gui Manager
+	uiman = gui.NewManager(gfx)
+	err := uiman.Initialize(gui.VertShader330, gui.FragShader330, int32(windowWidth), int32(windowHeight), int32(windowHeight))
+	if err != nil {
+		panic("Failed to initialize the user interface! " + err.Error())
+	}
+	guiinput.SetInputHandlers(uiman, mainWindow)
+
+	// load a font
+	_, err = uiman.NewFont("Default", fontFilepath, fontScale, fontGlyphs)
+	if err != nil {
+		panic("Failed to load the font file! " + err.Error())
+	}
+
+	/////////////////////////////////////////////////////////////////////////////
+	// make a window that will render the particle system
+	const particleWindowSize = 512
+	customMargin := mgl.Vec4{0, 0, 0, 0}
+	customWindowWS, customWindowHS := uiman.DisplayToScreen(particleWindowSize+8, particleWindowSize+8) // offset by 8 for windowPadding
+	customWS, customHS := uiman.DisplayToScreen(particleWindowSize, particleWindowSize)
+
+	renderer := forward.NewForwardRenderer(gfx)
+	renderer.ChangeResolution(particleWindowSize, particleWindowSize)
+	defer renderer.Destroy()
+
+	// load the diffuse shader
+	particleShader, err := fizzle.LoadShaderProgram(particles.VertShader330, particles.FragShader330, nil)
+	if err != nil {
+		panic("Failed to compile and link the particle shader program! " + err.Error())
+	}
+	defer particleShader.Destroy()
+
+	// create a particle system
+	particleSystem := particles.NewSystem(gfx)
+	emitter := particleSystem.NewEmitter(nil)
+	emitter.Properties.MaxParticles = 300
+	emitter.Properties.SpawnRate = 100
+	emitter.Properties.Size = 1.0
+	emitter.Properties.Color = mgl.Vec4{0.0, 0.9, 0.0, 1.0}
+	emitter.Properties.Velocity = mgl.Vec3{0, 1, 0}
+	emitter.Properties.Acceleration = mgl.Vec3{0, -0.1, 0}
+	emitter.Properties.TTL = 2.0
+	emitter.Shader = particleShader.Prog
+	emitter.Billboard, err = fizzle.LoadImageToTexture(billboardFilepath)
+	if err != nil {
+		panic("Failed to load the billboard texture: " + billboardFilepath + " " + err.Error())
+	}
+
+	// setup the camera to look at the cube
+	camera := fizzle.NewOrbitCamera(mgl.Vec3{0, 0, 0}, math.Pi/2.0, 5.0, math.Pi/2.0)
+
+	// now create the window itself
+	customWindow := uiman.NewWindow("Particle Output", 0.01, 0.99, customWindowWS, customWindowHS, func(wnd *gui.Window) {
+		wnd.Custom(customWS, customHS, customMargin, func() {
+			// rotate the cube and sphere around the Y axis at a speed of radsPerSec
+			gfx.ClearColor(0.0, 0.0, 0.0, 1.0)
+			gfx.Clear(graphics.COLOR_BUFFER_BIT | graphics.DEPTH_BUFFER_BIT)
+
+			perspective := mgl.Perspective(mgl.DegToRad(60.0), float32(particleWindowSize)/float32(particleWindowSize), 0.1, 50.0)
+			view := camera.GetViewMatrix()
+			particleSystem.Draw(perspective, view)
+		})
+	})
+	customWindow.Title = "Particle Output"
+	customWindow.ShowTitleBar = true
+	customWindow.Style.WindowPadding = mgl.Vec4{4, 4, 4, 4}
+
+	// create a window for editing the emitter properites
+	propertyWindow := uiman.NewWindow("Emitter", 0.66, 0.99, 0.33, 0.75, func(wnd *gui.Window) {
+		const textWidth = 0.33
+		props := &emitter.Properties
+
+		wnd.StartRow()
+		wnd.RequestItemWidthMin(textWidth)
+		wnd.Text("Max Particles")
+		wnd.DragSliderInt("maxparticles", 0.5, &props.MaxParticles)
+
+		wnd.StartRow()
+		wnd.RequestItemWidthMin(textWidth)
+		wnd.Text("Spawn Rate")
+		wnd.DragSliderUInt("spawnrate", 0.5, &props.SpawnRate)
+
+		wnd.StartRow()
+		wnd.RequestItemWidthMin(textWidth)
+		wnd.Text("TTL")
+		wnd.DragSliderFloat64("ttl", 0.1, &props.TTL)
+
+		wnd.StartRow()
+		wnd.RequestItemWidthMin(textWidth)
+		wnd.Text("Particle Size")
+		wnd.DragSliderFloat("size", 0.1, &props.Size)
+
+		wnd.StartRow()
+		wnd.RequestItemWidthMin(textWidth)
+		wnd.Text("Red")
+		wnd.SliderFloat("color1", &props.Color[0], 0.0, 1.0)
+
+		wnd.StartRow()
+		wnd.RequestItemWidthMin(textWidth)
+		wnd.Text("Green")
+		wnd.SliderFloat("color2", &props.Color[1], 0.0, 1.0)
+
+		wnd.StartRow()
+		wnd.RequestItemWidthMin(textWidth)
+		wnd.Text("Blue")
+		wnd.SliderFloat("color3", &props.Color[2], 0.0, 1.0)
+
+		wnd.StartRow()
+		wnd.RequestItemWidthMin(textWidth)
+		wnd.Text("Alpha")
+		wnd.SliderFloat("color4", &props.Color[3], 0.0, 1.0)
+	})
+	propertyWindow.Title = "Property Test"
+	propertyWindow.ShowTitleBar = true
+	propertyWindow.IsMoveable = true
+	propertyWindow.AutoAdjustHeight = true
+	//propertyWindow.ShowScrollBar = true
+	//propertyWindow.IsScrollable = true
+
+	/////////////////////////////////////////////////////////////////////////////
+	// loop until something told the mainWindow that it should close
+	// set some OpenGL flags
+	gfx.Enable(graphics.CULL_FACE)
+	gfx.Enable(graphics.DEPTH_TEST)
+	gfx.Enable(graphics.PROGRAM_POINT_SIZE)
+	gfx.Enable(graphics.BLEND)
+	gfx.BlendFunc(graphics.SRC_ALPHA, graphics.ONE_MINUS_SRC_ALPHA)
+
+	lastFrame := time.Now()
+	for !mainWindow.ShouldClose() {
+		// calculate the difference in time to control rotation speed
+		thisFrame := time.Now()
+		frameDelta := thisFrame.Sub(lastFrame).Seconds()
+
+		// update the data for the application
+		particleSystem.Update(frameDelta)
+
+		// clear the screen
+		//width, height := renderer.GetResolution()
+		gfx.Viewport(0, 0, int32(windowWidth), int32(windowHeight))
+		clearColor := gui.ColorIToV(114, 144, 154, 255)
+		gfx.ClearColor(clearColor[0], clearColor[1], clearColor[2], clearColor[3])
+		gfx.Clear(graphics.COLOR_BUFFER_BIT | graphics.DEPTH_BUFFER_BIT)
+
+		// draw the user interface
+		gfx.Disable(graphics.DEPTH_TEST)
+		gfx.Enable(graphics.SCISSOR_TEST)
+		uiman.Construct(frameDelta)
+		uiman.Draw()
+		gfx.Disable(graphics.SCISSOR_TEST)
+		gfx.Enable(graphics.DEPTH_TEST)
+
+		// draw the screen
+		mainWindow.SwapBuffers()
+
+		// advise GLFW to poll for input. without this the window appears to hang.
+		glfw.PollEvents()
+
+		// update our last frame time
+		lastFrame = thisFrame
+	}
+}
+
+// initGraphics creates an OpenGL window and initializes the required graphics libraries.
+// It will either succeed or panic.
+func initGraphics(title string, w int, h int) (*glfw.Window, graphics.GraphicsProvider) {
+	// GLFW must be initialized before it's called
+	err := glfw.Init()
+	if err != nil {
+		panic("Can't init glfw! " + err.Error())
+	}
+
+	// request a OpenGL 3.3 core context
+	glfw.WindowHint(glfw.Samples, 0)
+	glfw.WindowHint(glfw.ContextVersionMajor, 3)
+	glfw.WindowHint(glfw.ContextVersionMinor, 3)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+
+	// get a list of all the monitors to use and then take the one
+	// specified by the command line flag
+	monitors := glfw.GetMonitors()
+	if flagDesktopNumber >= len(monitors) {
+		flagDesktopNumber = -1
+	}
+	var monitorToUse *glfw.Monitor
+	if flagDesktopNumber >= 0 {
+		monitorToUse = monitors[flagDesktopNumber]
+	}
+
+	// do the actual window creation
+	mainWindow, err = glfw.CreateWindow(w, h, title, monitorToUse, nil)
+	if err != nil {
+		panic("Failed to create the main window! " + err.Error())
+	}
+	mainWindow.SetSizeCallback(onWindowResize)
+	mainWindow.MakeContextCurrent()
+
+	// disable v-sync for max draw rate
+	glfw.SwapInterval(0)
+
+	// initialize OpenGL
+	gfx, err := opengl.InitOpenGL()
+	if err != nil {
+		panic("Failed to initialize OpenGL! " + err.Error())
+	}
+	fizzle.SetGraphics(gfx)
+
+	return mainWindow, gfx
+}
+
+// onWindowResize is called when the window changes size
+func onWindowResize(w *glfw.Window, width int, height int) {
+	uiman.AdviseResolution(int32(width), int32(height))
+	//renderer.ChangeResolution(int32(width), int32(height))
+}
---
+++ particles/particles.go
@@ -0,0 +1,294 @@
+// Copyright 2016, Timothy` Bogdala <tdb@animal-machine.com>
+// See the LICENSE file for more details.
+
+package particles
+
+import (
+	"math"
+
+	mgl "github.com/go-gl/mathgl/mgl32"
+	graphics "github.com/tbogdala/fizzle/graphicsprovider"
+)
+
+/*
+
+* system
+  create(spawnerUpdate)
+  update()
+  [*] spawners
+      1 billboard image
+      1 array of particles
+      1 VBO data
+      1 shader program
+      0..1 collider
+
+      update(frameDelta)
+
+      stdParameters
+        * max particles
+        * spawn rate
+
+        * velocity
+        * acceleration
+        * time to live
+
+        ? spawn pattern
+*/
+
+var (
+	// VertShader330 is the GLSL vertex shader program for the basic particle emitter.
+	VertShader330 = `#version 330
+  uniform mat4 MVP;
+  in vec3 POSITION;
+  in vec4 COLOR;
+  in float SIZE;
+
+  out vec4 vs_color;
+
+  void main()
+  {
+    vs_color = COLOR;
+
+    gl_PointSize = SIZE;
+    gl_Position = MVP * vec4(POSITION, 1.0);
+  }`
+
+	// FragShader330 is the GLSL fragment shader program for the asic bparticle emitter.
+	FragShader330 = `#version 330
+  uniform sampler2D TEX;
+  in vec4 vs_color;
+
+  out vec4 frag_color;
+
+  void main()
+  {
+	frag_color = vs_color * texture(TEX, gl_PointCoord.st);
+  }`
+)
+
+// System is a particle system master collection that keeps track of all of the
+// particle emitters and updates them accordingly.
+type System struct {
+	Emitters []*Emitter
+	Origin   mgl.Vec3
+	gfx      graphics.GraphicsProvider
+	runtime  float64
+}
+
+// Emitter is a particle emmiter object that will keep track of all of the particles
+// created by the emitter and update them accordingly.
+type Emitter struct {
+	Owner      *System
+	Particles  []Particle
+	Billboard  graphics.Texture
+	Shader     graphics.Program
+	Properties EmitterProperties
+
+	vao            uint32
+	comboVBO       graphics.Buffer
+	comboBuffer    []float32
+	timeSinceSpawn float64
+}
+
+// EmitterProperties describes the behavior of an Emitter object and is it's own
+// type to facilitate sharing of parameter defaults and serialization.
+type EmitterProperties struct {
+	MaxParticles int
+	SpawnRate    uint // particles per second
+	Velocity     mgl.Vec3
+	Acceleration mgl.Vec3
+	TTL          float64  // in seconds
+	Origin       mgl.Vec3 // relative to Emitter.Owner.Origin
+	Color        mgl.Vec4
+	Size         float32
+}
+
+// Particle is an individual particle in an Emitter.
+type Particle struct {
+	Size         float32
+	Color        mgl.Vec4
+	Location     mgl.Vec3
+	Velocity     mgl.Vec3
+	Acceleration mgl.Vec3
+	EndTime      float64
+	StartTime    float64
+}
+
+// NewSystem creates a new particle system.
+func NewSystem(gfx graphics.GraphicsProvider) *System {
+	s := new(System)
+	s.gfx = gfx
+	return s
+}
+
+// GetTransform returns the transform matrix for the system as a whole.
+func (s *System) GetTransform() mgl.Mat4 {
+	return mgl.Translate3D(s.Origin[0], s.Origin[1], s.Origin[2])
+}
+
+// NewEmitter is it creates a new particle emitter and keeps track of it for
+// updating. An optional set of emitter properties can be specified.
+func (s *System) NewEmitter(optProps *EmitterProperties) *Emitter {
+	e := new(Emitter)
+	e.Owner = s
+
+	// set the emitter properties if specified
+	if optProps != nil {
+		e.Properties = *optProps
+	}
+
+	// construct the objects needed for rendering
+	e.vao = s.gfx.GenVertexArray()
+	e.comboVBO = s.gfx.GenBuffer()
+
+	// keep track of it
+	s.Emitters = append(s.Emitters, e)
+
+	return e
+}
+
+// Update will update all of the emitters currently tracked by the system.
+func (s *System) Update(frameDelta float64) {
+	s.runtime += frameDelta
+	for _, emitter := range s.Emitters {
+		emitter.Update(frameDelta)
+	}
+}
+
+// Draw renders all particle emitters.
+func (s *System) Draw(projection mgl.Mat4, view mgl.Mat4) {
+	for _, emitter := range s.Emitters {
+		emitter.Draw(projection, view)
+	}
+}
+
+// Update will update all of the particles for the emitter and then
+// update the graphics buffers.
+func (e *Emitter) Update(frameDelta float64) {
+	// filter out all of the dead particles
+	stillAlive := e.Particles[:0]
+	for _, particle := range e.Particles {
+		if e.Owner.runtime <= particle.EndTime {
+			stillAlive = append(stillAlive, particle)
+		}
+	}
+	e.Particles = stillAlive
+
+	// how many particle to spawn?
+	e.timeSinceSpawn += frameDelta
+	spawnInterval := 1.0 / float64(e.Properties.SpawnRate)
+	spawnCount := math.Floor(e.timeSinceSpawn / spawnInterval)
+
+	// update the timers
+	e.timeSinceSpawn -= spawnCount * spawnInterval
+
+	// update the particles
+	for i, particle := range e.Particles {
+		dV := particle.Velocity.Mul(float32(frameDelta))
+		dA := particle.Acceleration.Mul(float32(frameDelta))
+		e.Particles[i].Location = particle.Location.Add(dV)
+		e.Particles[i].Velocity = particle.Velocity.Add(dA)
+	}
+
+	// add the particles
+	var newParticle Particle
+	newParticle.StartTime = e.Owner.runtime
+	newParticle.Size = e.Properties.Size
+	newParticle.Color = e.Properties.Color
+	newParticle.Velocity = e.Properties.Velocity
+	newParticle.Acceleration = e.Properties.Acceleration
+	newParticle.Location = e.Owner.Origin.Add(e.Properties.Origin)
+	for spawnCount > 0 && len(e.Particles) < e.Properties.MaxParticles {
+		newParticle.EndTime = e.Properties.TTL + newParticle.StartTime
+		e.Particles = append(e.Particles, newParticle)
+		spawnCount--
+	}
+}
+
+const (
+	floatSize = 4
+)
+
+func (e *Emitter) renderToVBO() {
+	buffer := e.comboBuffer[:0]
+
+	for _, p := range e.Particles {
+		// 3f = vertex
+		buffer = append(buffer, p.Location[0])
+		buffer = append(buffer, p.Location[1])
+		buffer = append(buffer, p.Location[2])
+
+		// 4f = color
+		buffer = append(buffer, p.Color[0])
+		buffer = append(buffer, p.Color[1])
+		buffer = append(buffer, p.Color[2])
+		buffer = append(buffer, p.Color[3])
+
+		// 1f = size
+		buffer = append(buffer, p.Size)
+	}
+
+	// we didn't buffer anything
+	if len(buffer) <= 0 {
+		return
+	}
+
+	// buffer the data
+	e.Owner.gfx.BindBuffer(graphics.ARRAY_BUFFER, e.comboVBO)
+	e.Owner.gfx.BufferData(graphics.ARRAY_BUFFER, floatSize*len(buffer), e.Owner.gfx.Ptr(&buffer[0]), graphics.STREAM_DRAW)
+}
+
+// Draw renders the particle emitter.
+func (e *Emitter) Draw(projection mgl.Mat4, view mgl.Mat4) {
+	if e.Particles == nil || len(e.Particles) <= 0 {
+		return
+	}
+
+	gfx := e.Owner.gfx
+	gfx.BindVertexArray(e.vao)
+
+	// update the graphics buffers
+	e.renderToVBO()
+
+	gfx.UseProgram(e.Shader)
+
+	parentTransform := e.Owner.GetTransform()
+	modelTransform := mgl.Translate3D(e.Properties.Origin[0], e.Properties.Origin[1], e.Properties.Origin[2])
+	model := parentTransform.Mul4(modelTransform)
+	mvp := projection.Mul4(view).Mul4(model)
+
+	// bind the uniforms and attributes
+	mvpMatrix := gfx.GetUniformLocation(e.Shader, "MVP")
+	if mvpMatrix >= 0 {
+		gfx.UniformMatrix4fv(mvpMatrix, 1, false, mvp)
+	}
+
+	shaderTex0 := gfx.GetUniformLocation(e.Shader, "TEX")
+	if shaderTex0 >= 0 {
+		gfx.ActiveTexture(graphics.TEXTURE0)
+		gfx.BindTexture(graphics.TEXTURE_2D, e.Billboard)
+		gfx.Uniform1i(shaderTex0, 0)
+	}
+
+	const posOffset = 0
+	const colorOffset = floatSize * 3
+	const sizeOffset = floatSize * 7
+	const Stride = floatSize * (3 + 4 + 1) // vert / color / size
+
+	shaderPosition := gfx.GetAttribLocation(e.Shader, "POSITION")
+	gfx.BindBuffer(graphics.ARRAY_BUFFER, e.comboVBO)
+	gfx.EnableVertexAttribArray(uint32(shaderPosition))
+	gfx.VertexAttribPointer(uint32(shaderPosition), 3, graphics.FLOAT, false, Stride, gfx.PtrOffset(posOffset))
+
+	shaderColor := gfx.GetAttribLocation(e.Shader, "COLOR")
+	gfx.EnableVertexAttribArray(uint32(shaderColor))
+	gfx.VertexAttribPointer(uint32(shaderColor), 4, graphics.FLOAT, false, Stride, gfx.PtrOffset(colorOffset))
+
+	shaderSize := gfx.GetAttribLocation(e.Shader, "SIZE")
+	gfx.EnableVertexAttribArray(uint32(shaderSize))
+	gfx.VertexAttribPointer(uint32(shaderSize), 1, graphics.FLOAT, false, Stride, gfx.PtrOffset(sizeOffset))
+
+	gfx.DrawArrays(graphics.POINTS, 0, int32(len(e.Particles)*2))
+
+	gfx.BindVertexArray(0)
+}
